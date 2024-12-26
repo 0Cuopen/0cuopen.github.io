17396cefd021df7499eab87e689549ea3b76def6
0Cuopen <0cuopen@gmail.com>
2024-12-27 03:37:13 +0900

    test particles.js

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,3 @@
-particlesJS.load('particles-js', 'https://0cuopen.net/javascript/particles.json', function() {
+particlesJS.load('particles-js', '../javascript/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
